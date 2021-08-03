a5e545d7bb88b410c2358b5f49b97c3f21ea51ee
Jinyoung <roghabo@gmail.com>
2021-08-03 23:19:15 +0900

    to 4.7

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
-import { ApolloServer, gql } from "apollo-server";
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => "bebe",
-  },
-};
-
+const PORT = process.env.PORT;
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 });
-
 server
-  .listen()
-  .then(() => console.log("Server is running on http://localhost:4000/"));
+  .listen(PORT)
+  .then(() =>
+    console.log(`🚀Server is running on http://localhost:${PORT} ✅`)
+  );
